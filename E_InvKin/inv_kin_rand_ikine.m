9fcf00a18c5d1173612b86c97ac7efe046f5a8bd
JoshuaBRussell <theruss007@gmail.com>
2019-11-29 19:29:06 -0700

    Update to ot have fixed number for number of joints

--- E_InvKin/inv_kin_rand_ikine.m
+++ E_InvKin/inv_kin_rand_ikine.m
@@ -1,10 +1,14 @@
 function [sol_arr] = inv_kin_rand_ikine(R, T_des, q0,m);
 
-inv_q = zeros(50, 3);
-inv_q(1, 1:3) = q0;
-inv_q(1, 1:3) = R.ikine(T_des, rand(1,3).*q0, m);
-for i = 2:50
-    inv_q(i, 1:3) = R.ikine(T_des, -0.5+rand(1,3).*inv_q(i, 1:3), m);
+MaxIter = 50;
+num_of_joint_vars = length(R.config);
+
+inv_q = zeros(MaxIter, num_of_joint_vars);
+inv_q(1, 1:num_of_joint_vars) = q0;
+
+inv_q(1, 1:num_of_joint_vars) = R.ikine(T_des, rand(1,num_of_joint_vars).*q0, m);
+for i = 2:MaxIter
+    inv_q(i, 1:num_of_joint_vars) = R.ikine(T_des, -0.5+rand(1,num_of_joint_vars).*inv_q(i, 1:num_of_joint_vars), m);
 end
 
     revolute_mask = (R.config == 'R'); %Logical array for joints that are
