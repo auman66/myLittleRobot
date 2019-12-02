cf94ca503d7181e0e1adfb640dffeddd7bda229f
Alex Goldman <auman66@gmail.com>
2019-12-01 17:59:59 -0700

    expand funcationality for names in torqfung

--- dynamics/dyno_wrap.m
+++ dynamics/dyno_wrap.m
@@ -19,7 +19,7 @@ function [t, q, qd] = dyno_wrap(robot, t, torqfun, q0, qd0, varargin)
         qd0 = zeros(1,n);
     end
     %set contant torque function
-    if torqfun == 'constant'
+    if strcmp(torqfun,'constant')
         t_fun = @constant_torque;
         tau = varargin{1};
         %make sure input vector is correct size for constant_torque
@@ -29,8 +29,18 @@ function [t, q, qd] = dyno_wrap(robot, t, torqfun, q0, qd0, varargin)
             tau = [tau, zeros(1,n - length(tau))];
         end
         [t,q,qd] = robot.fdyn(t,t_fun,q0,qd0,tau);
-    elseif torqfun = 'PD'
+    elseif strcmp(torqfun,'PD')
+        %Need to convert from world space to joint space for qstar
         [t,q,qd] = robot.fdyn(t,@pd_controller,q0,qd0,varargin{:});
+    elseif ~isa(torqfun,'function_handle')
+        %clean up the input to remove any leading or trailing spaces
+        temp_fun = deblank(torqfun);  %remove trailing spaces
+        temp_flipped = temp_fun(end:-1:1);  %flip the string
+        temp_flipped = deblank(temp_flipped);  %remove trailing spaces of the flipped string
+        temp_fun = temp_flipped(end:-1:1);  %flip the string back
+        temp_fun = char(torqfun); %convert to a character array if it isn't already
+        t_fun = str2func(temp_fun)
+        [t,q,qd] = robot.fdyn(t,t_fun,q0,qd0,varargin{:});
     else
         [t,q,qd] = robot.fdyn(t,torqfun,q0,qd0,varargin{:});
     end
