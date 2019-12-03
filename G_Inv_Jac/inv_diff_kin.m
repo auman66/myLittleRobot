f02a343d5057cc8cffcc2b576870431c40e841ed
JoshuaBRussell <theruss007@gmail.com>
2019-12-02 19:39:31 -0700

    Add Inv Diff Kinematics

---
+++ G_Inv_Jac/inv_diff_kin.m
@@ -0,0 +1,13 @@
+function [q_dot] = inv_diff_kin(R, ve, q_pose)
+      %Inputs:
+      %ve - specifc end-effector velocity
+      %q_pose - specific pose the robot is in
+      
+      %Outputs:
+      %q_dot - time derivative to get ve at a specific pose
+
+       J = R.jacob0(q_pose);
+       q_dot = pinv(J) * ve;
+       
+end
+
