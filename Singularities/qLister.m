1428dc58d5c704b2cef92b1b2fd24a8c0082a09b
Alex Goldman <auman66@gmail.com>
2019-12-03 13:41:56 -0700

    comment and update singularity finder methods

--- Singularities/qLister.m
+++ Singularities/qLister.m
@@ -1,7 +1,17 @@
 function [q_list] = qLister(joint,div)
-%UNTITLED5 Summary of this function goes here
-%   Detailed explanation goes here
+% Returns a list of joint parameters to test in singularity finder
+% functions
+
+% Revolute Joint: first looks at range from -2pi to 2pi and then reduces to
+% be within joint limits, or leaves at -2pi to 2pi if none is available
+%
+% Prismatic Joint: finds values within range limit. Sets to -1 to 1 if not
+% limit is available. 
+
+
     qlim = joint.qlim;
+    
+    %revolute joint
     if joint.RP == 'R'
         step = 4*pi()/div;
         q_list = -2*pi():step:2*pi();
@@ -15,12 +25,15 @@ function [q_list] = qLister(joint,div)
             q_list = [q_list qlim];
             q_list = sort(unique(q_list));
         end
+        
+    %prismatic joint
     elseif joint.RP == 'P'
         if isempty(qlim)
             qlim = [-1 1];
         end
         step = (qlim(2)-qlim(1))/div;
         q_list = qlim(1):step:qlim(2);
+        %make sure 0, a typical singularity value, is on the list
         q_list = [q_list 0];
         q_list = sort(unique(q_list));
     else
